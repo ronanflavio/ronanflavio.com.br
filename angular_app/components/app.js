a38decbadceb117062d166dfb89640a832d0184b
Ronan Flávio Ribeiro <ronan.ribeiro@esportes.mg.gov.br>
2016-05-06 18:08:47 -0300

    new: creating angular app, css style and content

---
+++ angular_app/components/app.js
@@ -0,0 +1,35 @@
+var app = angular.module('app', ['ngMaterial', 'ngMessages', 'ngRoute']);
+
+app.config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('red');
+});
+
+app.controller('MainController', ['$scope', function ($scope) {
+    $scope.isOpen = false;
+    $scope.person = {
+        attr: [
+            {
+                key: 'Nome',
+                value: 'Ronan Flávio',
+            },
+            {
+                key: 'Idade',
+                value: '29',
+            },
+            {
+                key: 'Telefone',
+                value: '+55 31 98890-9120',
+            },
+            {
+                key: 'Email',
+                value: 'Ronan Flávio',
+            },
+            {
+                key: 'Localização',
+                value: 'Belo Horizonte, MG - Brasil',
+            },
+        ]
+    }
+}]);
